7c0a0bc9b454312c892efdc740a7f3f91dfbdbb8
Aritra-kayal <aritra9434853448@gmail.com>
2024-08-20 23:07:21 +0530

    factorial of a number

---
+++ Basic-C++/Exam-code-for-12th/factorial.cpp
@@ -0,0 +1,45 @@
+/*C++ program to find Factorial of a number using class*/
+#include <iostream>
+using namespace std;
+class factorial
+{
+private:
+    int n, n1, f;
+
+public:
+    void input();
+    void calc();
+    void display();
+};
+void factorial::input()
+{
+    cout << "\nEnter any number::";
+    cin >> n;
+}
+void factorial::calc()
+{
+    n1 = n;
+    f = 1;
+    if (n == 0 || n == 1)
+        cout << "\nFactorial of Number[" << n << "]is::1\n";
+    else
+    {
+        while (n > 0)
+        {
+            f = f * n;
+            n--;
+        }
+    }
+}
+void factorial::display()
+{
+    cout << "\nFactorial of[" << n1 << "]is::" << f << "\n";
+}
+int main()
+{
+    factorial f;
+    f.input();
+    f.calc();
+    f.display();
+    return 0;
+}
