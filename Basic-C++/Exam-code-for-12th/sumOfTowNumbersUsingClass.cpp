b74dca7d2aaeb8c4bb602fe9773c52faf8bc2ead
Aritra-kayal <aritra9434853448@gmail.com>
2024-08-20 20:46:14 +0530

    sum of two numbers

---
+++ Basic-C++/Exam-code-for-12th/sumOfTowNumbersUsingClass.cpp
@@ -0,0 +1,24 @@
+//Sum of two numbers using class
+#include<iostream>
+using namespace std;
+class Mathematics
+{
+    int x,y;
+    public:
+    void input()
+    {
+        cout<<"Input two integers\n";
+        cin>>x>>y;
+    }
+    void add()
+    {
+        cout<<"Result:"<<x+y;
+    }
+};
+int main()
+{
+    Mathematics m; // Creating an object of class
+    m.input();
+    m.add();
+    return 0;
+}
