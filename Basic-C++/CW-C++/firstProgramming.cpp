2bca4f14dfdf02218f16612e6d1c21eadad8b6d2
Aritra-kayal <aritra9434853448@gmail.com>
2024-10-15 11:10:48 +0530

    today's code

---
+++ Basic-C++/CW-C++/firstProgramming.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"My name is Aritra Kayal";
+    cout<<endl; //new line
+    cout<<"\n";
+    cout<<"Hello World";
+}
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"I am Aritra Kayal"<<endl<<"\n"<<"Hello World";
+// }
+//a good thing
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    cout<<4;
+//    cout<<endl;
+//    cout<<"4";
+//    cout<<endl;
+//    cout<<4+3;
+//    cout<<endl;
+//    cout<<"4+3";
+//    cout<<endl; 
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int X;
+//     X=5;
+//     cout<<X;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     x=5;
+//     cout<<x+5;
+// }
+
---
+++ Basic-C++/CW-C++/sumOfTwoNumber.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x=5;
+    int y;
+    y=5;
+    cout<<x+y;
+}
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;//declaration
+//     x=5;//initialization
+//     y=5;
+//     cout<<x+y;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;//both
+//     int y=6;
+//     cout<<x+y;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=7;
+//     cout<<x+y;
+// }
